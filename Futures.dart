9b7d75f41496715d848118a9ef1cfed623da5bb5
Harmanjot Singh <narulasahil2002@gmail.com>
2026-08-17 13:19:00 +0530

    fetching data

---
+++ Futures.dart
@@ -0,0 +1,28 @@
+void main() async {
+  print('i am at main');
+
+  fetchPost().then((p) {
+    print(p.title);
+    print(p.userId);
+  });
+
+  // via async and await
+  final post = await fetchPost();
+  print(post.title);
+  print(post.userId);
+}
+
+Future<Post> fetchPost() {
+  const delay = Duration(seconds: 3);
+
+  return Future.delayed(delay, () {
+    return Post('my post', 123);
+  });
+}
+
+class Post {
+  String title;
+  int userId;
+
+  Post(this.title, this.userId);
+}
